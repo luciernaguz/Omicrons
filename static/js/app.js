349f2f856b4ec38b70366557be5506097360b975
luciernaguz <luciernaguz@hotmail.com>
2021-02-09 18:45:40 -0600

    Update

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,45 @@ console.log('Ok connected 23 - app.js');
 let jsonData = '../Data/nfl_teams.json'; // (ref 2)
 let jsonStadiums='../Data/stadiums_nfl.json';
 
+// Icon
+let Icon = L.icon({
+            iconUrl: '../images/logo1.png',
+            shadowUrl: '../images/logo2.png',
+            iconSize:     [30, 48], // size of the icon
+            shadowSize:   [25, 20], // size of the shadow
+            iconAnchor:   [16, 80], // point of the icon which will correspond to marker's location
+            shadowAnchor: [12, 50],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+  // Create the tile layer that will be the background of our map
+  let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 20,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  });
+
+  // Create a baseMaps object to hold the lightmap layer
+  let baseMaps = {
+    "Light Map": lightmap
+  };
+
+  // Create an overlayMaps object to hold the bikeStations layer
+  /* let overlayMaps = {
+    "NFL Stadiums": Stadiums
+  }; */
+    
+  // Create the map object with options
+  let map = L.map("map-id", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [lightmap]
+  });
+
+  let Markers= L.marker();
+
+  let Stadiums =L.marker();
 
 /**
  * Execute init functions
@@ -77,8 +116,12 @@ function optionChanged(selectedTeam){
                     Type: ${teams[x].StadiumDetails.Type} <br>
                 `);
 
-                let newMarker=L.marker([teams[x].StadiumDetails.GeoLat, teams[x].StadiumDetails.GeoLong]);
-                //function(createMap(newMarker))
+                Markers.remove()
+                Stadiums.remove()
+                Markers=L.marker([teams[x].StadiumDetails.GeoLat, teams[x].StadiumDetails.GeoLong],{icon:Icon}).addTo(map);
+                
+                //d3.select('#map-id').addLayer(stadiumMarkers);     
+                           
                 //.bindPopup("<h3>" + stadium.Name + "<h3><h3>Capacity: " + stadium.Capacity + "</h3><h3>Type: " + stadium.Type + "</h3>");
             }
         }
@@ -88,76 +131,46 @@ function optionChanged(selectedTeam){
 //Map Base
 function createMarkers() {
     d3.json(jsonStadiums).then((data) => {
-        console.log(data)
+        //console.log(data)
         // Pull the "stadiums" property off of response.data
-        var stadiums = data;
-        console.log(stadiums);
+        let stadiums = data;
+        //console.log(stadiums);
 
         // Initialize an array to hold stadiums markers
-        let stadiumMarkers = [];
+        stadiumMarkers = [];
     
-        // Icon
-        var Icon = L.icon({
-        iconUrl: '../images/logo1.png',
-        shadowUrl: '../images/logo2.png',
-        iconSize:     [33, 60], // size of the icon
-        shadowSize:   [30, 30], // size of the shadow
-        iconAnchor:   [18, 90], // point of the icon which will correspond to marker's location
-        shadowAnchor: [14, 60],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-        });
+
     
         // Loop through the stadiums array
         for (var index = 0; index < stadiums.length; index++) {
             var stadium = stadiums[index];
             console.log(stadium);
-            var sMarker=L.marker([stadium.GeoLat, stadium.GeoLong], {icon:Icon})
+            // For each stadium, create a marker and bind a popup with the stadium's name
+            let sMarker=L.marker([stadium.GeoLat, stadium.GeoLong], {icon:Icon})
             .bindPopup("<h3>" + stadium.Name + "<h3><h3>Capacity: " + stadium.Capacity + "</h3><h3>Type: " + stadium.Type + "</h3>");
             //console.log(sMarker);    
             stadiumMarkers.push(sMarker);
-            console.log(stadiumMarkers);
-        // For each stadium, create a marker and bind a popup with the stadium's name
+            //console.log(stadiumMarkers);
+        
         }
 
-        createMap(L.layerGroup(stadiumMarkers));         
+        Stadiums=L.layerGroup(stadiumMarkers).addTo(map);
+        console.log(Stadiums);
  });
 } 
 
-function createMap(Stadiums) {
 
-    // Create the tile layer that will be the background of our map
-    let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.light",
-      accessToken: API_KEY
-    });
+
   
-    // Create a baseMaps object to hold the lightmap layer
-    let baseMaps = {
-      "Light Map": lightmap
-    };
+   
   
-    // Create an overlayMaps object to hold the bikeStations layer
-    let overlayMaps = {
-      "NFL Stadiums": Stadiums
-    };
-  
-    // Create the map object with options
-    let map = L.map("map-id", {
-      center: [37.09, -95.71],
-      zoom: 5,
-      layers: [lightmap, Stadiums]
-    });
-  
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    //Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
     // L.control.layers(baseMaps, overlayMaps, {
     //   collapsed: false
-    // }).addTo(map);
-    
+    // }).addTo(map);    
 
     //Stadiums =L.marker([39.09, -84.51]).addTo(map);
-}
+
 
 //Execute init fuctions
 init();
